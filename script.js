e2c09ecac27be17bd38fc21844d6986114694b58
ayush <ayush.arya.16@gmail.com>
2026-01-13 09:29:54 +0000

    filter list added. assets added

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ extra_slots.forEach(es => {
 })
 
 updateSlots()
+// buff filter
+
+fetch("./data/buffs.json").then(res => res.json()).then(buffs => createBuffFilters(buffs));
+
+function createBuffFilters(buffs) {
+    const container = document.getElementById("filters");
+
+    Object.entries(buffs).sort((a, b) => {
+        const labelA = a[1].label.toLowerCase();
+        const labelB = b[1].label.toLowerCase();
+        return labelA.localeCompare(labelB);
+    }).forEach(([id, buff]) => {
+        console.log(id, buff)
+
+        const label = document.createElement("label");
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.value = id
+
+        const text = document.createTextNode(" " + buff.label)
+        label.style.display = "block"
+
+        label.appendChild(checkbox)
+        label.appendChild(text)
+
+        container.appendChild(label);
+    })
+}
